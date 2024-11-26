d626c05d4e21a9602eeba516eaf736640ac1ce11
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2024-11-26 12:49:12 -0300

    Update InicializarUsuarios.java

--- src/main/java/br/com/aceleraprogramador/gerenciamento_pedidos/security/InicializarUsuarios.java
+++ src/main/java/br/com/aceleraprogramador/gerenciamento_pedidos/security/InicializarUsuarios.java
@@ -20,49 +20,22 @@ public class InicializarUsuarios implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Usuario usuario = criarPerfilUsuario();
-        Usuario gerente = criarPerfilGerente();
-        Usuario administrador = criarPerfilAdministrador();
-        List<Usuario> usuarios = List.of(usuario, gerente, administrador);
-        usuarioRepository.saveAll(usuarios);
+        Usuario usuario = criarPerfilDeUsuario("Usuario", "password", "usuario@usuario.com", Role.ROLE_USUARIO);
+        Usuario gerente = criarPerfilDeUsuario("Gerente", "password", "gerente@gerente.com", Role.ROLE_GERENTE);
+        Usuario administrador = criarPerfilDeUsuario("Administrador", "password", "administrador@administrador.com", Role.ROLE_ADMINISTRADOR);
+        usuarioRepository.saveAll(List.of(usuario, gerente, administrador));
     }
 
-    private Usuario criarPerfilAdministrador() {
+    private Usuario criarPerfilDeUsuario(String nome, String senha, String email, Role role) {
         Usuario administrador = new Usuario();
-        administrador.setNome("Administrador");
-        administrador.setSenha(passwordEncoder.encode("password"));
+        administrador.setNome(nome);
+        administrador.setSenha(passwordEncoder.encode(senha));
         ModelRole roleAdministrador = ModelRole.
                 builder()
-                .name(Role.ROLE_ADMINISTRADOR)
+                .name(role)
                 .build();
         administrador.setRoles(List.of(roleAdministrador));
-        administrador.setEmail("administrador@administrador.com");
+        administrador.setEmail(email);
         return administrador;
-    }
-
-    private Usuario criarPerfilGerente() {
-        Usuario gerente = new Usuario();
-        gerente.setNome("Gerente");
-        gerente.setSenha(passwordEncoder.encode("password"));
-        ModelRole roleGerente = ModelRole.
-                builder()
-                .name(Role.ROLE_GERENTE)
-                .build();
-        gerente.setRoles(List.of(roleGerente));
-        gerente.setEmail("gerente@gerente.com");
-        return gerente;
-    }
-
-    private Usuario criarPerfilUsuario() {
-        Usuario usuario = new Usuario();
-        usuario.setNome("Usuario");
-        usuario.setSenha(passwordEncoder.encode("password"));
-        ModelRole roleUsuario = ModelRole.
-                builder()
-                .name(Role.ROLE_USUARIO)
-                .build();
-        usuario.setRoles(List.of(roleUsuario));
-        usuario.setEmail("usuario@usuario.com");
-        return usuario;
     }
 }
